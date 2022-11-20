485cb011537b96a8f2d72e202756a3265cd19d1a
Sıla Kaçmaz <sila.kacmaz@obase.com>
2022-11-20 13:25:44 +0300

    model oluşturuldu

---
+++ lib/models/pizza_model.dart
@@ -0,0 +1,21 @@
+import 'package:equatable/equatable.dart';
+import 'package:flutter/material.dart';
+
+class Pizza extends Equatable {
+  String? id;
+  String? name;
+  Image? image;
+
+  Pizza({required this.id, required this.name, required this.image});
+
+  @override
+  List<Object?> get props => [id, name, image];
+
+  static List<Pizza> pizzas = [
+    Pizza(id: '0', name: 'Pizza', image: Image.asset('images/pizza.png')),
+    Pizza(
+        id: '1',
+        name: 'Pizza Pepperoni',
+        image: Image.asset('images/pizza_pepperoni.png')),
+  ];
+}
