3e87e9c714ef6fff59e795820f1f0fca3fc2985f
Sıla Kaçmaz <sila.kacmaz@obase.com>
2022-11-20 13:36:25 +0300

    state ve event oluşturuldu

---
+++ lib/bloc/pizza_bloc.dart
@@ -0,0 +1,15 @@
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+
+import '../models/pizza_model.dart';
+
+part 'pizza_event.dart';
+part 'pizza_state.dart';
+
+class PizzaBloc extends Bloc<PizzaEvent, PizzaState> {
+  PizzaBloc() : super(PizzaInitial()) {
+    on<PizzaEvent>((event, emit) {
+      // TODO: implement event handler
+    });
+  }
+}
